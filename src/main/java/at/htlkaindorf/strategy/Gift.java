c2fcf41dd700247ef11166dd8bed94ea7eb81dcd
MrKuh <michi.wajand@gmail.com>
2022-06-16 23:49:06 +0200

    Some fixes and sounds

--- src/main/java/at/htlkaindorf/strategy/Gift.java
+++ src/main/java/at/htlkaindorf/strategy/Gift.java
@@ -12,6 +12,8 @@ public abstract class Gift {
     protected Action action = new NixAction();
     protected Animation animation = new NixAnimation();
 
+    protected boolean spawned = false;
+
     protected int x, y;
 
     protected double speed;
@@ -19,6 +21,18 @@ public abstract class Gift {
     protected int spriteCounter = 0;
 
     protected Rectangle hitBox;
+
+
+    public void spawn() {
+        spawned = true;
+    }
+    public void update() {
+        if(spawned){
+
+        }
+
+    }
+
 
     public void setAction(Action action) {
         this.action = action;
