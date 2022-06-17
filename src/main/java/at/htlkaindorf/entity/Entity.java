9d2d8373be4cf3d175605541cdeb69e0a554e7af
benefitzer <renbea18@htl-kaindorf.at>
2022-06-17 14:29:42 +0200

    documentation + questionBlock

--- src/main/java/at/htlkaindorf/entity/Entity.java
+++ src/main/java/at/htlkaindorf/entity/Entity.java
@@ -7,18 +7,39 @@ import lombok.RequiredArgsConstructor;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+
+/**
+ * This {@code Entity} class is used to give {@code PurpleMonster} it's required instances through inheritance.
+ * @author MrKuh
+ * @author Bensi
+ * @version 1.1
+ */
 @Data
 @RequiredArgsConstructor
 @AllArgsConstructor
-
 public class Entity {
+    /**
+     * These variables are used to identify the current Position in a 2D coordinate system
+     */
     protected int x, y;
+    /**
+     * This variable defines the speed of an {@code Entity} object.<br>
+     * This speed decreases the x constant so the {@code Entity} will fly to the {@code Player}.
+     */
     protected double speed;
-
+    /**
+     * Just an object of {@code GamePanel}. <br>
+     * It is needed if u want to call a function of {@code GamePanel}.
+     */
     protected GamePanel gamePanel;
 
+    /**
+     * This variable is used...
+     */
     protected int spriteCounter = 0;
-
+    /**
+     * This {@code Rectangle} object is used to define the HitBox of the {@code Entity}
+     */
     protected Rectangle hitBox;
 
     public Entity(GamePanel gamePanel) {
